068a87a7328ea87e78b496b7c7e48b5b342cfff1
Eljay02 <202312200@fit.edu.ph>
2024-11-28 14:51:27 +0800

    commit

--- script.js
+++
@@ -1,8 +0,0 @@
-
-const hamburger = document.getElementById('hamburger');
-const sideMenu = document.getElementById('side-menu');
-
-
-hamburger.addEventListener('click', () => {
-    sideMenu.classList.toggle('open');
-});
